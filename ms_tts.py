cb834f0527b8bd0651eea8c8f09562f4ef7fb9cc
fffnower <32289652+fffnower@users.noreply.github.com>
2023-05-27 13:40:19 +0800

    Update ms_tts.py

--- ms_tts.py
+++ ms_tts.py
@@ -21,20 +21,17 @@ async def tts(TEXT):
     async for chunk in communicate.stream():
         if chunk["type"] == "audio":
             audio_stream.write(chunk["data"])
-        # elif chunk["type"] == "WordBoundary":
-        #     print(f"WordBoundary: {chunk}")
     audio_stream.seek(0)
     audio = AudioSegment.from_file(audio_stream)
     return audio
 
 # 消费音频文件
 def play_sound():
-    # 用一个循环来等待事件
     while True:
         audio = sound_queue.get()
         play(audio)
 
-# 判断是否tts句子是否是纯标点符号组成的
+# 判断tts文本是否由纯标点符号组成
 def is_punctuation(s):
     # 定义中文标点符号
     # chinese_punctuation = "，。、；：！？《》（）【】‘’“”"
@@ -46,6 +43,7 @@ def is_punctuation(s):
 
 # 主程序
 def main(tts_length):
+    # 交互
     txt_files = []
     idx = 0
     for filename in os.listdir():
@@ -53,24 +51,26 @@ def main(tts_length):
             txt_files.append(filename)
             print(f'{idx}. {filename}')
             idx += 1
-
     selection = input('Enter the number of the file you want to read: ')
     selected_file = txt_files[int(selection)]
 
-    # 检测文件编码
+    # 检测文件编码，检测头部32个字符
     with open(selected_file, 'rb') as f:
         result = chardet.detect(f.read(32))
     encoding = result['encoding']
     print(f'selected file:{selected_file}\nfile Encoding: {encoding}\n')
 
+    # 读取数据并处理
     tmp_tts_text = ''
     with codecs.open(selected_file, 'r', encoding=encoding) as file:
         while True:
             chunk = file.read(tts_length)
             if not chunk:
                 break
+            # 用标点符号，将文字分段
             for idx, c in enumerate(chunk[::-1]):
                 if c in ",;:!?，。；：！？\n":
+                    # idx为零，及第一个字符就是标点符号，特殊处理
                     if idx:
                         tts_text = tmp_tts_text + chunk[:-idx]
                         tmp_tts_text = chunk[-idx:]
@@ -93,10 +93,10 @@ def main(tts_length):
             print(tmp_tts_text)
 
 if __name__ == '__main__':
-    # 队列最大容量，默认为1
+    # 队列最大容量，1即可保证连续无停顿
     queue_size = 1
-    # 单次tts文本长度，默认为300字符
-    tts_length = 300
+    # 单次tts文本长度，太小可能无法分段处理，语音会有停顿
+    tts_length = 512
 
     # 创建一个共享的队列来存储音频文件
     sound_queue = queue.Queue(maxsize=queue_size)
